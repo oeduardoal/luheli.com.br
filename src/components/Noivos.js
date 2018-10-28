4a6d6672987dfaa28703f93a5533ae9af93fa8eb
Eduardo Almeida <oeduardoal@gmail.com>
2018-10-28 20:53:26 -0300

    estu

--- src/components/Noivos.js
+++ src/components/Noivos.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import noivos from "../assets/eles.jpg";
 import bg from "../assets/bg.jpg";
@@ -36,27 +36,71 @@ const Descricao = styled.p`
   margin-top: 10px;
   text-align: center;
 `;
+window.now = new Date();
 
-const Noivos = () => (
-  <Container banner={bg}>
-    <NoivosImg src={noivos} />
-    <NomeNoivos>Elielson e Luana</NomeNoivos>
-    <Descricao>
-      Por trás de um grande amor, <br />
-      existe uma grande história!
-    </Descricao>
-    <br />
-    <br />
-    <Descricao>
-      <strong>Local: Rua Ceará 535, Centro-Eusébio - CE</strong>
-      <br />
-      <br />
-      <strong>Horário: às 09:00</strong>
-      <br />
-      <br />
-      <strong>Traje: Esporte Fino</strong>
-    </Descricao>
-  </Container>
-);
+export default class Noivos extends Component {
+  state = {};
 
-export default Noivos;
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    console.log("asddas");
+
+    let countDownDate = new Date("Nov 24, 2018 09:00:00").getTime();
+
+    let x = setInterval(function() {
+      let now = new Date().getTime();
+
+      let distance = countDownDate - now;
+
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      document.getElementById("contagem-regressiva-js").innerHTML =
+        days +
+        " Dias " +
+        hours +
+        " Horas " +
+        minutes +
+        " Minutos " +
+        seconds +
+        "s ";
+
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("contagem-regressiva-js").innerHTML =
+          "JÁ É HOJE! #luheli";
+      }
+    }, 1000);
+  };
+
+  render() {
+    return (
+      <Container banner={bg}>
+        <NoivosImg src={noivos} />
+        <NomeNoivos>Elielson e Luana</NomeNoivos>
+        <Descricao>
+          Por trás de um grande amor, <br />
+          existe uma grande história!
+        </Descricao>
+        <Descricao>
+          <h1 id="contagem-regressiva-js">Carregando...</h1>
+          <br />
+          <strong>Local: Rua Ceará 535, Centro-Eusébio - CE</strong>
+          <br />
+          <br />
+          <strong>Horário: 24/11/2018 - às 09:00</strong>
+          <br />
+          <br />
+          <strong>Traje: Esporte Fino</strong>
+        </Descricao>
+      </Container>
+    );
+  }
+}
